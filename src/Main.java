72d4d735fea7d41e015d7212b781e73af0eb956e
martincorradetti <153010947+martincorradetti@users.noreply.github.com>
2024-01-09 11:27:45 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -37,4 +37,4 @@ public class Main {
         System.out.println(ticket);
 
     }
-}
+}*/
